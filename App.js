f60f55ddf616221ffcc684ca1d27922b9ba7bfca
yushan <yushanwebdev@gmail.com>
2022-09-17 21:26:08 +0530

    feat: Configure the `post` prop in the `<FeedPost />` Component

--- App.js
+++ App.js
@@ -2,10 +2,26 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import FeedPost from "./src/components/FeedPost";
 
+const post1 = {
+  id: "p1",
+  createdAt: "19 m",
+  User: {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    name: "Yushan Fernando",
+  },
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <FeedPost />
+      <FeedPost post={post1} />
 
       <StatusBar style="auto" />
     </View>
--- src/components/FeedPost.js
+++ src/components/FeedPost.js
@@ -7,23 +7,7 @@ import {
 } from "@expo/vector-icons";
 import LikeImage from "../../assets/images/like.png";
 
-const post = {
-  id: "p1",
-  createdAt: "19 m",
-  User: {
-    id: "u1",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-    name: "Yushan Fernando",
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-  numberOfLikes: 11,
-  numberOfShares: 2,
-};
-
-const FeedPost = () => {
+const FeedPost = ({ post }) => {
   return (
     <View style={styles.post}>
       {/* Header */}
