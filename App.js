28e40bf6887d5ddd87eaf2f06c8ad59fa06843b1
yushan <yushanwebdev@gmail.com>
2022-09-20 21:15:36 +0530

    feat: Configure the Amplify Studio - Configure the Amplify in the `<App />`

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./src/navigation";
+import awsconfig from "./src/aws-exports";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   return (
