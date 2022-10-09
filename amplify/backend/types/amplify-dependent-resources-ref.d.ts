003878411b7631b4a02fda451b4a6e94bd533803
yushanwebdev <yushanwebdev@gmail.com>
2022-10-09 17:37:27 +0530

    feat: Storage - Add the Amplify Storage Configurations

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3notjustfriendsstoragefbe5eb76": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
